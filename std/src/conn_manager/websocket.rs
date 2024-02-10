60863c4261f5d15904df09a87c26f3cbde00922f
Miles Murgaw <milesmurgaw@gmail.com>
2024-02-10 16:59:29 -0500

    feat: start of websocket conn

--- std/src/conn_manager/websocket.rs
+++ std/src/conn_manager/websocket.rs
@@ -1,21 +1,48 @@
 use super::{ConnError, Connection, Message};
 use async_trait::async_trait;
+use dioxus::hooks::{UnboundedReceiver, UnboundedSender};
+use futures::channel::mpsc;
+use futures_util::{SinkExt, StreamExt};
+use wasm_bindgen::{closure::Closure, JsCast};
+use web_sys::MessageEvent;
 
-pub struct Websocket {}
+pub struct WebsocketClient {
+    reciever: UnboundedReceiver,
+}
 
-impl Websocket {
+impl WebsocketClient {
     pub fn connect(url: &str) -> Self {
-        todo!()
+        //TODO: Handle errors
+        let ws = web_sys::Websocket::new(url).unwrap();
+
+        let (tx, rx) = mpsc::unbounded();
+
+        let cl = Closure::wrap(Box::new(move |e: MessageEvent| {
+            if let Ok(text) = e.data().dyn_into::<js_sys::JsString>() {
+                let string: String = text.into();
+                // TODO: Handle error
+                let val = serde_json::from_str::<Message>(&string).unwrap();
+                // TODO: Handle error
+                tx.unbounded_send(val).unwrap();
+            }
+        }) as Box<dyn FnMut(MessageEvent)>);
+
+        ws.set_onmessage(Some(cl.as_ref().unchecked_ref()));
+        cl.forget();
+
+        Self {
+            reciever: rx,
+        }
     }
 }
 
 #[async_trait]
-impl Connection for Websocket {
+impl Connection for WebsocketClient {
     fn send(&self, msg: Message) -> Result<(), ConnError> {
         todo!()
     }
 
     async fn recv(&self) -> Message {
-        todo!()
+        self.reciever.next().await
     }
 }
