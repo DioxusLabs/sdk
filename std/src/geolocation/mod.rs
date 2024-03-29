25e6b2a9766327a621860c37e0e1c96e39ebcd23
Evan Almloff <evanalmloff@gmail.com>
2024-03-28 21:27:57 -0500

    address comments

--- std/src/geolocation/mod.rs
+++ std/src/geolocation/mod.rs
@@ -1,14 +1,12 @@
-#[cfg(any(windows, target_family = "wasm"))]
-mod core;
-#[cfg(any(windows, target_family = "wasm"))]
-mod platform;
-#[cfg(any(windows, target_family = "wasm"))]
-mod use_geolocation;
-
-#[cfg(any(windows, target_family = "wasm"))]
-pub use self::core::*;
-#[cfg(any(windows, target_family = "wasm"))]
-pub use self::use_geolocation::*;
-
-#[cfg(not(any(windows, target_family = "wasm")))]
-compile_error!("The geolocation module is not supported on this platform.");
+cfg_if::cfg_if! {
+    if #[cfg(any(windows, target_family = "wasm"))] {
+        pub mod core;
+        pub mod platform;
+        pub mod use_geolocation;
+        pub use self::core::*;
+        pub use self::use_geolocation::*;
+    }
+    else {
+        compile_error!("The geolocation module is not supported on this platform.");
+    }
+}
