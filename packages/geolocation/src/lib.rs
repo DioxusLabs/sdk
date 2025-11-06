8bf2a1a1b8cf72f6c6fa6859380ed95dd27ffe32
Jonathan Kelley <jkelleyrtp@gmail.com>
2025-11-06 17:52:04 -0300

    remove compile error

--- packages/geolocation/src/lib.rs
+++ packages/geolocation/src/lib.rs
@@ -1,15 +1,7 @@
 //! Interact with location services.
 
-cfg_if::cfg_if! {
-    if #[cfg(any(windows, target_family = "wasm"))] {
-        pub mod core;
-        pub mod platform;
-        pub mod use_geolocation;
-        pub use self::core::*;
-        pub use self::use_geolocation::*;
-    }
-
-    else {
-        compile_error!("the `geolocation` feature is only available on wasm and windows targets");
-    }
-}
+pub mod core;
+pub mod platform;
+pub mod use_geolocation;
+pub use self::core::*;
+pub use self::use_geolocation::*;
--- packages/geolocation/src/platform/mod.rs
+++ packages/geolocation/src/platform/mod.rs
@@ -1,9 +1,46 @@
-cfg_if::cfg_if! {
-    if #[cfg(windows)] {
-        mod windows;
-        pub use self::windows::*;
-    } else if #[cfg(target_family = "wasm")] {
-        mod wasm;
-        pub use self::wasm::*;
+#[cfg(windows)]
+mod windows;
+#[cfg(windows)]
+pub use self::windows::*;
+
+#[cfg(target_family = "wasm")]
+mod wasm;
+#[cfg(target_family = "wasm")]
+pub use self::wasm::*;
+
+#[cfg(not(any(target_family = "wasm", windows)))]
+mod unsupported {
+    use std::sync::Arc;
+
+    use crate::{Error, Event, Geocoordinates};
+
+    pub struct Geolocator {}
+
+    impl Geolocator {
+        /// Create a new Geolocator for the device.
+        pub fn new() -> Result<Self, Error> {
+            Err(Error::Unsupported)
+        }
+    }
+
+    pub async fn get_coordinates(_geolocator: &Geolocator) -> Result<Geocoordinates, Error> {
+        Err(Error::Unsupported)
+    }
+
+    pub fn set_power_mode(
+        _geolocator: &mut Geolocator,
+        _power_mode: crate::PowerMode,
+    ) -> Result<(), Error> {
+        Err(Error::Unsupported)
+    }
+
+    pub fn listen(
+        _geolocator: &Geolocator,
+        _callback: Arc<dyn Fn(Event) + Send + Sync>,
+    ) -> Result<(), Error> {
+        Err(Error::Unsupported)
     }
 }
+
+#[cfg(not(any(target_family = "wasm", windows)))]
+pub use self::unsupported::*;
